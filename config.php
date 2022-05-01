da83a711eb8428855a31c975c2f1bf17ef46e0cc
mhamzaq869 <mhamzaq869@gmail.com>
2022-05-01 03:13:17 -0500

    initial

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "indiepro_superadmin";
+// MySQL Database Password
+$sql_db_pass = "TeeIsVV*~.u(";
+// MySQL Database Name
+$sql_db_name = "indiepro_prodgdbs";
+
+// Site URL
+$site_url = "https://prodg.indiepro.io"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "56dfd620-1f23-4e20-9c6e-23dc9fbc672d"; // Your purchase code, don't give it to anyone. 
+?>
